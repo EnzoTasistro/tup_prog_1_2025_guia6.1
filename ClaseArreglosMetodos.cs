832bf8d0a01be1fa9302ce4fca6aa64bf246aed7
EnzoTasistro <tasistroenzo18@gmail.com>
2025-05-30 09:02:11 -0300

    Add files via upload

---
+++ ClaseArreglosMetodos.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int [] vector = {4,11,18,25};          //definir vector los corchetes [] indican que es un vector, luego se indica la variable
+            // los valores indican todos los domingos de este mes, para saber que numero caen (CREARLO POR VALOR)
+
+            //int[] vector = new int[30];            //Estoy creando un vector de 30 lugares (se inicializa en  0)
+
+
+            int [] datos; //creo el vector
+            int tamaño;   //creo una variable para guardar la cantidad de arcivos que entraran en el vector
+
+            Console.WriteLine("Ingrese el tamaño de el vector"); //pedir el valor del vector al usuario
+            tamaño = Convert.ToInt32(Console.ReadLine());        //el usuario ingresa el valor y se convierte a INT
+
+            datos = new int[tamaño];      //se asigna el nuevo valor a datos en base a lo que ingreso el usuario
+
+            for (int i = 0; i < tamaño; i++)    //se usa un for para ir contando la cantidad de lugares ocupados en el vector dependiendo el tamaño ingresado por el usuario
+            {
+
+                Console.Write($"Ingresa el {i + 1} valor");        //se pide el valor a guardar en el vector
+                datos[i] = Convert.ToInt32(Console.ReadLine());  //se ingresa el valor y se asigna al espacio del vector
+
+            }
+
+            Console.WriteLine($"El promedio es {Promedio(datos):f2}");
+            Console.WriteLine($"El promedio de los 3 primeros es {Promedio(datos)}");
+            Console.WriteLine($"El mayor ingresado es {VerMayor(datos)}");
+
+            Console.ReadKey();
+
+        }
+
+        static double Promedio(int[] auxiliar) //se crea el metodo y se llama al vector (se llama distinto pero apunta a la misma direccion de memoria)
+        {
+
+            double resultado = 0;
+
+            for (int i = 0; i < auxiliar.Length; i++)
+            { 
+            
+                resultado = resultado + auxiliar[i];
+
+            }
+
+            return resultado / auxiliar.Length;  //El metodo siempre se finaliza con un return (el resultado que se devuelve)
+        }
+
+        static double Promedio(int[] auxiliar, int cantidad)
+        {
+            double resultado = 0;
+            int i;
+
+            for (i = 0; i < cantidad; i++)
+            {
+
+                resultado = resultado + auxiliar[i];
+
+
+            }
+
+            return resultado / cantidad;
+
+        }
+
+        static int VerMayor(int[] pepe)
+        {
+
+            int mayor = pepe[0];
+            for (int i = 1; i < pepe.Length; i++)
+            {
+
+                if (pepe[i] > mayor)
+                mayor = pepe[i];
+               
+            }
+            return mayor;
+        }
+    }
+}
